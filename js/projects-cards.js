de1b1361fd4ed54b2f72630b74af71fa4661654f
FabioDevCode <0677384849@orange.fr>
2022-03-25 20:46:00 +0100

    fonction de tri des projets + rectification textes

--- js/projects-cards.js
+++ js/projects-cards.js
@@ -2,6 +2,10 @@ import { projets } from './data-projets.js';
 const blocProject = document.querySelector('.bloc-projet');
 
 showProjectCards()
+sourtOutProject()
+
+const allProjetsBtn = document.querySelector('.so1');
+allProjetsBtn.click()
 
 function showProjectCards() {
 
@@ -10,7 +14,7 @@ function showProjectCards() {
         let card = document.createElement('div');
         card.classList.add("projet");
         card.classList.add(`${projets[i].type}`);
-        card.classList.add("active");
+        card.classList.add("none");
         card.innerHTML = `
             <img src="./assets/images/projects/${projets[i].image}.jpg" alt="photo d'un projet">
             <a href="projet.html?${projets[i].id}">${projets[i].title}</a>
@@ -19,3 +23,89 @@ function showProjectCards() {
         blocProject.appendChild(card);
     };
 };
+
+function sourtOutProject() {
+    const btnSourter = document.querySelectorAll('.sourt_out');
+
+    btnSourter.forEach(btn => {
+
+        btn.addEventListener('click', function() {
+
+            btnSourter.forEach(btn => {
+                btn.classList.remove('active');
+            })
+            this.classList.add('active');
+
+            const btnSourted = (Array.from(this.classList)).slice(1)[0];
+
+            displayCardProject(btnSourted)
+        });
+    });
+};
+
+function displayCardProject(sourtedOut) {
+    const allProjets = document.querySelectorAll('.projet');
+
+    switch(sourtedOut) {
+        case 'so1':
+            allProjets.forEach(projets => {
+                projets.classList.remove('none');
+            });
+            break;
+        case 'so2':
+            const medico = document.querySelectorAll('.MEDICO');
+            hideProject();
+            medico.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+        case 'so3':
+            const educat = document.querySelectorAll('.EDUCATION');
+            hideProject();
+            educat.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+        case 'so4':
+            const equipe = document.querySelectorAll('.EQUIPEMENTS');
+            hideProject();
+            equipe.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+        case 'so5':
+            const logements = document.querySelectorAll('.LOGEMENTS');
+            hideProject();
+            logements.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+        case 'so6':
+            const maison = document.querySelectorAll('.MAISON');
+            hideProject();
+            maison.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+        case 'so7':
+            const commerce = document.querySelectorAll('.COMMERCE');
+            hideProject();
+            commerce.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+        case 'so8':
+            const indus = document.querySelectorAll('.INDUSTRIEL');
+            hideProject();
+            indus.forEach(projets => {
+                projets.classList.remove('none');
+            })
+            break;
+    };
+
+    function hideProject() {
+        allProjets.forEach(projets => {
+            projets.classList.add('none');
+        })
+    };
+}
