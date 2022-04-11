001f6c70ae62eba991325775f2d3937a35407c29
FabioDevCode <0677384849@orange.fr>
2022-04-11 20:18:05 +0200

    Amélioration menu Tablette

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ const MenuText = document.querySelector('.textmenu');
 const FermerText = document.querySelector('.textfermer');
 const header = document.querySelector('header');
 const divLogo = document.queryCommandValue('.logo');
+const main = document.querySelector('main');
+
+const cruxBtn = document.createElement('div');
+cruxBtn.classList.add('crux');
+main.appendChild(cruxBtn)
 
 btnMenu.addEventListener('click', function() {
     btnMenu.classList.toggle('active');
@@ -29,4 +34,52 @@ btnMenu.addEventListener('click', function() {
         MenuText.style.display = "flex";
         FermerText.style.display = "none";
     }
-})
+
+    if((Array.from(this.classList)).slice(1)[0]) {
+        setTimeout(function() {
+            cruxBtn.style.right = '0px';
+            console.log("c'est ouvert");
+            window.addEventListener('scroll', noScroll);
+        }, 100);
+    } else {
+        setTimeout(function() {
+            cruxBtn.style.right = '-100vw';
+            console.log("C'est fermé");
+            window.removeEventListener('scroll', noScroll);
+        }, 200);
+    };
+});
+
+cruxBtn.addEventListener('click', function() {
+    btnMenu.classList.toggle('active');
+
+    if(btnMenu.classList[1]) {
+        header.style.left = '-84px';
+        svgBtn.style.tranform = 'rotate(720deg)';
+        svgBarre.style.display = "none";
+        svgCroix.style.display = "flex";
+        MenuText.style.display = "none";
+        FermerText.style.display = "flex";
+    } else {
+        header.style.left = '-168px';
+        svgBtn.style.tranform = 'rotate(-720deg)';
+        svgBarre.style.display = "flex";
+        svgCroix.style.display = "none";
+        MenuText.style.display = "flex";
+        FermerText.style.display = "none";
+    }
+
+    if((Array.from(this.classList)).slice(1)[0]) {
+        setTimeout(function() {
+            cruxBtn.style.right = '0px';
+            console.log("c'est ouvert");
+            body.style.overflow = 'hidden';
+        }, 100);
+    } else {
+        setTimeout(function() {
+            cruxBtn.style.right = '-100vw';
+            console.log("C'est fermé");
+            body.style.overflow = 'auto';
+        }, 200);
+    };
+});
